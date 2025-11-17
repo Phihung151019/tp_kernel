2c045c0026ac5be747cc99c128a798a036b5e1d7
phihung151019 <trantrongphihung1992@gmail.com>
2025-11-17 19:18:11 +0700

    PhiHung

---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+./clean.sh
+
+echo -e "\n[INFO]: BUILD STARTED..!\n"
+
+#init submodules
+git submodule init && git submodule update
+
+export KERNEL_ROOT="$(pwd)/kernel_platform/common"
+export ARCH=arm64
+export KBUILD_BUILD_USER="@sagar"
+
+# Install the requirements for building the kernel when running the script for the first time
+if [ ! -f ".requirements" ]; then
+    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
+        default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
+        python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
+        make repo cpio kmod openssl libelf-dev pahole libssl-dev libarchive-tools zstd --fix-missing && wget http://security.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2ubuntu0.1_amd64.deb && sudo dpkg -i libtinfo5_6.3-2ubuntu0.1_amd64.deb && touch .requirements
+fi
+
+# Create necessary directories
+mkdir -p "${KERNEL_ROOT}/out" "${KERNEL_ROOT}/build"
+
+# Export toolchain paths
+export PATH="${KERNEL_ROOT}/toolchains/clang-r416183b/bin:${PATH}"
+export LD_LIBRARY_PATH="${KERNEL_ROOT}/toolchains/clang-r416183b/lib64:${LD_LIBRARY_PATH}"
+
+# Set cross-compile environment variables
+export BUILD_CROSS_COMPILE="${KERNEL_ROOT}/toolchains/gcc/bin/aarch64-none-linux-gnu-"
+export BUILD_CC="${KERNEL_ROOT}/toolchains/clang-r416183b/bin/clang"
+
+BUILD_TARGET=b0q_gbl_openx
+export MODEL=$(echo ${BUILD_TARGET} | cut -d'_' -f1)
+export PROJECT_NAME=${MODEL}
+export REGION=$(echo ${BUILD_TARGET} | cut -d'_' -f2)
+export CARRIER=$(echo ${BUILD_TARGET} | cut -d'_' -f3)
+
+export TARGET_BUILD_VARIANT= user
+CHIPSET_NAME=waipio
+export ANDROID_BUILD_TOP=$(pwd)
+export TARGET_PRODUCT=gki
+export TARGET_BOARD_PLATFORM=gki
+
+export ANDROID_PRODUCT_OUT=${ANDROID_BUILD_TOP}/out/target/product/${MODEL}
+export OUT_DIR=${ANDROID_BUILD_TOP}/out/msm-${CHIPSET_NAME}-${CHIPSET_NAME}-${TARGET_PRODUCT}
+
+# for Lcd(techpack) driver build
+export KBUILD_EXTRA_SYMBOLS=${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mmrm-driver/Module.symvers
+
+# for Audio(techpack) driver build
+export MODNAME=audio_dlkm
+
+export KBUILD_EXT_MODULES="../vendor/qcom/opensource/datarmnet-ext/wlan                           ../vendor/qcom/opensource/datarmnet/core                           ../vendor/qcom/opensource/mmrm-driver                           ../vendor/qcom/opensource/audio-kernel                           ../vendor/qcom/opensource/camera-kernel                           ../vendor/qcom/opensource/display-drivers/msm                         "
+
+# Build options for the kernel
+export BUILD_OPTIONS="
+-C ${KERNEL_ROOT} \
+O=${KERNEL_ROOT}/out \
+-j$(nproc) \
+ARCH=arm64 \
+LLVM=1 \
+LLVM_IAS=1 \
+CROSS_COMPILE=${BUILD_CROSS_COMPILE} \
+CC=${BUILD_CC} \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+"
+
+build_kernel(){
+    # Make default configuration.
+    # Replace 'your_defconfig' with the name of your kernel's defconfig
+    make ${BUILD_OPTIONS} tp_defconfig
+
+    # Configure the kernel (GUI)
+    # make ${BUILD_OPTIONS} menuconfig
+
+    # Build the kernel
+    make ${BUILD_OPTIONS} Image || exit 1
+
+    # Copy the built kernel to the build directory
+    cp "${KERNEL_ROOT}/out/arch/arm64/boot/Image" "${KERNEL_ROOT}/build"
+
+    echo -e "\n[INFO]: BUILD FINISHED..!"
+}
+build_kernel
